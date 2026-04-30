faf79aa5d2c1e4e171b95ca7266b8dd02a50320b
wikawikunia11 <usiamal@gmail.com>
2026-04-30 15:07:44 +0200

    Added dependencies, CorsConfig and postgresql DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, price) VALUES ('Carrot', 1.99);
+INSERT INTO products (name, price) VALUES ('Cucumber', 0.89);
+INSERT INTO products (name, price) VALUES ('Flour', 4.99);
+INSERT INTO products (name, price) VALUES ('Milk', 2.15);
+INSERT INTO products (name, price) VALUES ('Orange Juice', 8.99);
+INSERT INTO products (name, price) VALUES('Honey', 24.99);
